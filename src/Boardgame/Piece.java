767c0c1e50d67e1fa3c3751fda27d42cfd70dfb1
Arthur Estrela <arthurestrela369@gmail.com>
2024-07-23 15:30:46 -0300

    Little improvement in board printing

--- src/Boardgame/Piece.java
+++ src/Boardgame/Piece.java
@@ -11,8 +11,4 @@ public class Piece {
     protected Board getBoard() {
         return board;
     }
-
-    
-    
-
 }
--- src/Boardgame/Position.java
+++ src/Boardgame/Position.java
@@ -28,8 +28,5 @@ public class Position {
     @Override
     public String toString() {
         return row + ", " + column;
-    }
-
-    
-    
+    } 
 }
